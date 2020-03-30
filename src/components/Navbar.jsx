d1ebb1ae407659452c02351d6af055afd83889b0
Marco <marcosiegman01@gmail.com>
2020-03-29 18:18:22 -0600

    added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "../assets/img/logo.svg";
+export default () => {
+  return (
+    <header
+      class="
+      flex justify-between items-center
+      p-3 w-screen 
+      fixed top-0 left-0 
+      bg-secondary  shadow-2xl"
+    >
+      <img
+        src={logo}
+        class="h-6 object-contain cursor-pointer hover:opacity-25 transition-opacity ease-in duration-300"
+        alt=""
+      />
+      <div class="text-white text-2xl cursor-pointer hover:opacity-25 transition-opacity ease-in duration-300 mr-4">
+        &equiv;
+      </div>
+    </header>
+  );
+};
