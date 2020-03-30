d99f368c3c8f86b124e4b1cff2c69025fd07ad93
Marco <marcosiegman01@gmail.com>
2020-03-29 22:10:47 -0600

    fixed styles on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,19 +33,26 @@ export default () => {
           opacity: isSearchShowed ? 1 : 0,
           height: isSearchShowed ? "4rem" : 0
         }}
-        className="mt-2 flex transition-all  duration-200 ease-out overflow-hidden"
+        className="mt-2 flex transition-all duration-200 ease-out overflow-hidden w-11/12
+                  lg:w-4/12
+        "
       >
         <input
           type="text"
           placeholder="Search nice food"
-          className="py-1 px-2 text-gray-900 rounded-lg text-sm border-b-4 border-transparent transition transition-colors duration-200 ease-in
+          className="
+            w-4/6
+            py-1 px-2 text-gray-900 
+            rounded-lg text-sm border-b-4 
+            border-transparent 
+            transition-colors duration-200 ease-in
            focus:border-primary
            transition-opacity duration-200 ease-linear 
           "
         />
         <button
           type="submit"
-          className="rounded-md flex hover:opacity-50 transition-opacity duration-200 ease-linear justify-center items-center bg-primary w-12 h-8 ml-2"
+          className="rounded-md flex hover:opacity-50 transition-opacity duration-200 ease-linear justify-center items-center bg-primary w-2/6 h-8 ml-2"
         >
           <i className="fa fa-search text-white" />
         </button>
