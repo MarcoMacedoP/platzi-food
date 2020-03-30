a83d4b34173df1d0bbd7b6ab230dbcf24a9a0d0c
Marco <marcosiegman01@gmail.com>
2020-03-29 22:17:54 -0600

    Updates

---
+++ precache-manifest.65671c8d2802027dabaf2f066c2a2c10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca9038016ffe5f932fd6ba3234a4eb9",
+    "url": "/platzi-food/index.html"
+  },
+  {
+    "revision": "a5a762fc2cf55223681c",
+    "url": "/platzi-food/static/css/main.f2fbe301.chunk.css"
+  },
+  {
+    "revision": "07bc5a2a3c1fd7ddde93",
+    "url": "/platzi-food/static/js/2.ad796dbf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/platzi-food/static/js/2.ad796dbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a762fc2cf55223681c",
+    "url": "/platzi-food/static/js/main.7a7ab78e.chunk.js"
+  },
+  {
+    "revision": "63d1941276656ffcd6bc",
+    "url": "/platzi-food/static/js/runtime-main.6ca3c254.js"
+  },
+  {
+    "revision": "c5309bae0b5fa5d5f41d8bce0506330c",
+    "url": "/platzi-food/static/media/logo.c5309bae.svg"
+  }
+]);
