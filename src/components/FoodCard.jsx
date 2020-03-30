09cc6ca844714b121cb7f09c54a9d29d571d6143
Marco <marcosiegman01@gmail.com>
2020-03-29 21:46:55 -0600

    added search bar on navbar

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -12,20 +12,22 @@ export default ({
     <section
       className="
       flex flex-col mb-2 
-      md:w-2/6 md:mx-2
-      lg:w-3/12 
+      md:w-2/6 md:mx-2 mb-6
+      lg:w-3/12
+      cursor-pointer
+      hover:opacity-50 transition transition-opacity duration-500 ease-linear
       "
     >
       <img
         className="rounded-lg mb-3 w-full h-full object-cover 
-        lg:max-h-40 "
+        lg:h-40 "
         src={image}
         alt=""
       />
-      <div className="p-4 flex flex-col relative -mt-10 bg-white rounded-lg shadow-md max-h-full ">
+      <div className="p-4 flex flex-col relative -mt-10 bg-white rounded-lg shadow-md">
         <h2 className="text-xl font-medium text-gray-800 ">{title}</h2>
         <p className="text-sm text-gray-600">{desc}</p>
-        <div className="text-sm my-2 text-gray-500">MXN {price}$</div>
+        <div className="text-sm text-gray-500">MXN {price}$</div>
         <div className="flex justify-center flex-col items-start text-sm  ">
           <div>
             {activeStars.map((e, i) => (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,55 @@
 import React from "react";
 import logo from "../assets/img/logo.svg";
 export default () => {
+  const [isSearchShowed, setIsSearchShowed] = React.useState(false);
   return (
     <header
-      class="
-      flex justify-between items-center
-      p-3 w-screen 
-      z-50
-      fixed top-0 left-0 
-      bg-secondary  shadow-2xl"
+      className="navbar md:px-12"
+      style={{ height: isSearchShowed ? "7rem" : "4rem" }}
     >
-      <img
-        src={logo}
-        class="h-6 object-contain cursor-pointer hover:opacity-25 transition-opacity ease-in duration-300"
-        alt=""
-      />
-      <div class="text-white text-2xl cursor-pointer hover:opacity-25 transition-opacity ease-in duration-300 mr-4">
-        &equiv;
-      </div>
+      <nav
+        className="flex w-full transi justify-between h-full items-center
+      "
+      >
+        <img
+          src={logo}
+          className="h-6 object-contain cursor-pointer hover:opacity-25 transition-opacity ease-in duration-300"
+          alt=""
+        />
+        <div
+          onClick={() => setIsSearchShowed(!isSearchShowed)}
+          className="text-white text-2xl cursor-pointer hover:opacity-25  transition-opacity ease-in duration-300 mr-4"
+        >
+          {isSearchShowed ? (
+            <i class="fa fa-times"></i>
+          ) : (
+            <i className="fa fa-bars" />
+          )}
+        </div>
+      </nav>
+
+      <form
+        style={{
+          opacity: isSearchShowed ? 1 : 0,
+          height: isSearchShowed ? "4rem" : 0
+        }}
+        className="mt-2 flex transition-all  duration-200 ease-out overflow-hidden"
+      >
+        <input
+          type="text"
+          placeholder="Search nice food"
+          className="py-1 px-2 text-gray-900 rounded-lg text-sm border-b-4 border-transparent transition transition-colors duration-200 ease-in
+           focus:border-primary
+           transition-opacity duration-200 ease-linear 
+          "
+        />
+        <button
+          type="submit"
+          className="rounded-md flex hover:opacity-50 transition-opacity duration-200 ease-linear justify-center items-center bg-primary w-12 h-8 ml-2"
+        >
+          <i className="fa fa-search text-white" />
+        </button>
+      </form>
     </header>
   );
 };
